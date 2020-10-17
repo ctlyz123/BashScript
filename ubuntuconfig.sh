549cc57549d223d9ac232e7673d2a9ade582c49b
chentongle <17865513701@163.com>
2020-10-18 00:26:12 +0800

    add ubuntuconfig.sh jenv

--- ubuntuconfig.sh
+++ ubuntuconfig.sh
@@ -2,16 +2,22 @@
  
 # change apt sources list
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
-sed -i "s/cn.archive.ubuntu.com/mirrors.tuna.tsinghua.edu.cn/g" /etc/apt/sources.list
-apt update 
+
+version=`cat /etc/issue| head -n 1 | awk '{print $2}' | awk -F '.' '{print $1"."$2}'`
+
+if [ "$version" = "20.04" ]; then
+	sed -i "s/cn.archive.ubuntu.com/mirrors.tuna.tsinghua.edu.cn/g" /etc/apt/sources.list
+fi
+
+if [ "$version" = "16.04" ]; then
+	sed -i "s/archive.ubuntu.com/mirrors.tuna.tsinghua.edu.cn/g" /etc/apt/sources.list
+fi
+	apt update 
 
 
 # install softwares
 
-apt install net-tools tmux vim zsh git ssh -y
-
-
-
+apt install net-tools tmux vim zsh git ssh python curl zip -y
 
 # change config file 
 sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config
@@ -22,7 +28,16 @@ service ssh restart
 
 # oh-my-zsh
 wget http://129.226.182.204/install.sh -O - | sh;chsh -s /bin/zsh 
-su - test -c "wget http://129.226.182.204/install.sh -O - | sh;chsh -s /bin/zsh "
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+su - test -c "wget http://129.226.182.204/install.sh -O - | sh;chsh -s /bin/zsh;git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions;source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh"
+
+# autojump
+cd /home/
+git clone https://github.com/ctlyz123/autojump.git
+cd autojump
+./install.py
+echo '[[ -s ~/.autojump/etc/profile.d/autojump.sh ]] && . ~/.autojump/etc/profile.d/autojump.sh' >> ~/.zshrc
 
 # oh-my-tmux
 cd
@@ -41,6 +56,19 @@ else
 fi 
 
 # install jenv
+
+read -p "install jenv?[y/n](default n):" jenvflag
+
+if [ "n" = "$jenvflag" -o "" = "$jenvflag" ]; then  
+  echo "skip install docker"  
+else  
+	git clone https://github.com/ctlyz123/jenv.git ~/.jenv
+	echo 'export PATH="$HOME/.jenv/bin:$PATH"' >> ~/.zshrc
+	echo 'eval "$(jenv init -)"' >> ~/.zshrc
+fi 
+
+
+
 
 
 
